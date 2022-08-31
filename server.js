c9fe5b6d42dd0d246fe33040b3972c4dba436321
Drew Anello <drew.anello@gmail.com>
2022-08-30 21:21:48 -0400

    adds array to drinks.js

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 
 
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
-
 })
-
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
+module.exports = drinks
 
 app.listen(port)
 console.log('up and running in port', port)
