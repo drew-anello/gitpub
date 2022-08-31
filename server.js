34f8117525b7af5ca5e520b50bcd7c4bf79af00e
Drew Anello <drew.anello@gmail.com>
2022-08-30 22:25:28 -0400

    Page showing

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ const drinks = require('./models/drinks')
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
-app.get('/drinks', (req, res) => {
-    res.send(drinks)
-})
+// app.get('/drinks', (req, res) => {
+//     res.send(drinks)
+// })
+app.get("/drinks", (req, res) => {
+    res.render('drinks_index.ejs', {
+        drinks, 
+    });
+});
+
+app.get("/drinks/:id", (req, res) => {
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.id],
+    });
+});
 
 app.listen(port)
 console.log('up and running in port', port)
