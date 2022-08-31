bac06107ae50e52731e1ea962778c13e6d5b9a79
Drew Anello <drew.anello@gmail.com>
2022-08-30 20:07:52 -0400

    Adds app.get and listen

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = express()
 const port = 3000
+const app = express()
+
+
+app.get("/fruits/", (req, res) => {
+    res.send(fruits)
+})
+
+
+app.listen(3000, () => {
+    console.log("listening")
+})
